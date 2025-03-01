c7efcf85e1265dbcb0db2545e596b1541e53dcdb
enda-quantmatix <enda.carroll@quantmatix.com>
2025-03-01 17:13:05 +0000

    Adding env variables

--- shell/env.sh
+++ shell/env.sh
@@ -1,1 +1,7 @@
 # This is for common/generic environment variables, paths etc
+
+# Updates to PATH
+export PATH="$PATH:"
+
+# Coloured warnings for GCC
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
