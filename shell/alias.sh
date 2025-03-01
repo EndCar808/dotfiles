395c49d6607a2bff639a322473686cbc015a6bde
enda-quantmatix <enda.carroll@quantmatix.com>
2025-03-01 16:54:47 +0000

    Added to generic alias.sh

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,1 +1,21 @@
 # This is for common/generic alias commands
+alias ls='ls -alF --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Git shortcuts
+alias gst='git status'
+alias gpl='git pull'
+alias gp='igt push'
+alias gpr='git pull --rebase'
+alias gaa='git add .'
+alias gc='git commit'
+alias gco='git checkout'
+alias gb='git branch'
+
+# Editor
+export EDITOR="nvim"
+
+# Other generic commands
+alias h='history'
