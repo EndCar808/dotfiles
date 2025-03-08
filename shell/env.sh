c41952375b3c90fcc3e69ec15df7317246ae7d57
enda-quantmatix <enda.carroll@quantmatix.com>
2025-03-08 21:25:14 +0000

    Added setup for obsidian

--- shell/env.sh
+++ shell/env.sh
@@ -11,6 +11,12 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 # Format for time
 export TIMEFMT=$'\n================\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
 
+# Setting the GUI as Xserver - needs to be activated on Windows
+export DISPLAY=$(grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0
+
+# setting dbus -> needed for Obsidian
+sudo service dbus start >/dev/null 2>&1
+
 # # Bash configs
 # if [ -n "$BASH_VERSION" ]; then
 
