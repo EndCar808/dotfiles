76a83f61e8e625b484a5c920fbaeefd7ef11130e
enda-quantmatix <enda.carroll@quantmatix.com>
2025-12-03 18:45:08 +0000

    Add keymap for saving file in insert mode that puts you back in insert mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Override saving in INSERT mode - leaves you in INSERT mode
+vim.keymap.set("i", "<C-s>", "<C-o>:w<CR>", { desc = "Save File (stay in insert)" })
